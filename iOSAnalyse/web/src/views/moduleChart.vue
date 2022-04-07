2d480c75033bf6eb1ca4a6b91fdec682a10006a0
sandy's <wcf1654@126.com>
2022-04-07 15:59:51 +0800

    Update moduleChart.vue

--- iOSAnalyse/web/src/views/moduleChart.vue
+++ iOSAnalyse/web/src/views/moduleChart.vue
@@ -30,7 +30,7 @@ export default {
     drawLineChart() {
       this.chartLine = echarts.init(this.$el, 'shine');
       this.modules.forEach(module => {
-          this.xAxis.unshift(module.publish.lver)
+          this.xAxis.unshift(module.publish[0].version)
           this.yAxis.unshift(module.module_size)
         })
 

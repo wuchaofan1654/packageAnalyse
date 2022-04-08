b151848ee950e54590e3edceaad8cfd7f45e4e71
sandy's <wcf1654@126.com>
2022-04-08 10:54:21 +0800

    Update request.js
    change base_url

--- iOSAnalyse/web/src/utils/request.js
+++ iOSAnalyse/web/src/utils/request.js
@@ -7,7 +7,7 @@ axios.defaults.headers["Content-Type"] = "application/json;charset=utf-8";
 // 创建axios实例
 const service = axios.create({
   // axios中请求配置有baseURL选项，表示请求URL公共部分
-  baseURL:  'http://120.53.121.82/',  // process.env.VUE_APP_BASE_API,
+  baseURL:  'http://120.53.121.82:8080/',  // process.env.VUE_APP_BASE_API,
   // 超时
   timeout: 10000
 });

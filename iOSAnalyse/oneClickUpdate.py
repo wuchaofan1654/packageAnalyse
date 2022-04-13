e94162329a92a07e0c6003c2a303b3f9f705372c
西格玛（吴超凡） <wuchaofan@soyoung.com>
2022-04-13 11:40:03 +0800

    增加一键更新

---
+++ iOSAnalyse/oneClickUpdate.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Create by sandy at 11:12 13/04/2022
+Description: ToDo
+"""
+from iOSAnalyse.settings import BASE_DIR
+import os
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class OneClickUpdate(object):
+
+    def __init__(self):
+        result = os.popen(f'git pull').read()
+        logger.info(result)
+        self.uwsgi_file_path = os.path.join(BASE_DIR, 'packageAnalyse', 'iOSAnalyse')
+        self.update_server()
+        self.update_web()
+
+    def update_server(self):
+        result = os.popen(f"pkill -9 uwsgi && uwsgi --plugin python3 {self.uwsgi_file_path}")
+        logger.info(result.read())
+
+    @classmethod
+    def update_web(cls):
+        result = os.popen(f'cd {BASE_DIR} && cd web/ && npm run build && \\cp dist/ /var/www/site/ && nginx -s reload')
+        logger.info(result.read())
+
+
+OneClickUpdate()
